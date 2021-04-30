b8ede30b6e8c547e9a1db25d87f30578090a8dfe
efthimia145 <efthimia145@gmail.com>
2021-05-01 01:13:53 +0300

    Added working but not finalized implementation of conv decoder

--- gr-a3sat/lib/conv_dec_impl.h
+++ gr-a3sat/lib/conv_dec_impl.h
@@ -15,15 +15,15 @@ namespace gr {
     class conv_dec_impl : public conv_dec
     {
         /*!
-         * \param rate The number of the generator polynomials used for the encoding.
-         * \param constraint_length The length of each generator polynomial used for the encoding.
+         * \param rate Number of the generator polynomials used for the encoding.
+         * \param constraint_length Length of each generator polynomial used for the encoding.
          */
      private:
-        unsigned int rate;
-        unsigned int constraint_length;
+        int rate;
+        int constraint_length;
 
-     public:
-      conv_dec_impl(unsigned int rate, unsigned int constraint_length);
+    public:
+      conv_dec_impl(int rate, int constraint_length);
       ~conv_dec_impl();
 
       int general_work(int noutput_items,
--- gr-a3sat/python/qa_conv_dec.py
+++ gr-a3sat/python/qa_conv_dec.py
@@ -24,18 +24,16 @@ class qa_conv_dec(gr_unittest.TestCase):
 
     def test_001_t(self):
         # set up fg
-        src_data = (0, 1, 1, 0, 0, 1)
-        expected_result = (1, 0, 1)
+        src_data = (1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0)
+        # src_data = (1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0)
+        expected_result = (1, 0, 1, 1, 0, 0)
         conv_dec = a3sat.conv_dec(True)
         src = blocks.vector_source_b(src_data, False, 1, [])
         self.tb.connect(src, conv_dec)
-        print("connected!")
         #sqr = conv_enc.test_001_conv_enc()
         dst = blocks.vector_sink_b()
         self.tb.connect(conv_dec, dst)
-        print(" connected!!")
         self.tb.run()
-        print("OK")
         result_data = dst.data()
         self.assertTupleEqual(expected_result, result_data, "test failed")
         # check data
