2a51bc5f6d473ff883e1dbf5cfdd849439690d92
Manolis Mylonas <mylowade98@gmail.com>
2021-05-10 15:51:35 +0300

    Tests

--- prototypes/LDPC/hex2binary.m
+++ prototypes/LDPC/hex2binary.m
@@ -49,8 +49,8 @@ for i=1:length(final_binary_matrix(:,1))
     temp = find(final_binary_matrix(i,:)==1)+(column_control)*size_quasi_cycle; 
     Rows_Parity_bits = [Rows_Parity_bits temp];
 end
-Rows_Parity_bits = Rows_Parity_bits(2:end);% - matlab_to_cpp;
-position_Rows = position_Rows(2:end);% - matlab_to_cpp;
+Rows_Parity_bits = Rows_Parity_bits(2:end) - matlab_to_cpp;
+position_Rows = position_Rows(2:end) - matlab_to_cpp;
 
 %full_Rows_Parity_bits = 0;
 
--- prototypes/LDPC/test_encoder.m
+++ prototypes/LDPC/test_encoder.m
@@ -1,9 +1,10 @@
 hex2binary
 
-message = ones(4096,1);
+message = (rand(4096,1) > 0.5);
 out = encoder_ldpc_test(message, Generator_Matrix);
-
+out = out(4097:end);
 
 function [encoded_message] = encoder_ldpc_test(message, Generator)
 encoded_message = message'*Generator;
+encoded_message = mod(encoded_message,2);
 end
