095dc1a5674854e73213a3547f45c30063890489
efthimia145 <efthimia145@gmail.com>
2021-09-23 22:40:59 +0300

    Add extra test for the decoder

--- gr-a3sat/python/qa_conv_dec.py
+++ gr-a3sat/python/qa_conv_dec.py
@@ -26,10 +26,10 @@ class qa_conv_dec(gr_unittest.TestCase):
         self.tb = None
 
     """
-    This function tests the functionality of the convolutinal decoder block.
+    This function tests the functionality of the convolutional decoder block.
     It writes a stream of 2048 random generated bits in a file. 
     Encodes them using the convolutional encoder block. 
-    Modifies the written stream by flipping bits in random positions. 
+    Modifies the written stream by flipping 1 bit in random position. 
     Then decodes the modified stream and compares it to the initial one. 
     """
     def test_001_conv_dec(self):
@@ -56,9 +56,50 @@ class qa_conv_dec(gr_unittest.TestCase):
         self.tb.run()
         encoded_message = encoder_output.data()
 
-        corrupted_message = self.flip_bits(encoded_message, 2)
+        corrupted_message = self.flip_bits(encoded_message, 1)
 
     # Decode the message
+        conv_dec = a3sat.conv_dec(False)
+        src = blocks.vector_source_b(corrupted_message, False, 1, [])
+        self.tb.connect(src, conv_dec)
+        dst = blocks.vector_sink_b()
+        self.tb.connect(conv_dec, dst)
+        print("Run")
+        self.tb.run()
+        result_data = dst.data()
+        print(result_data)
+        self.assertTupleEqual(initial_message, result_data, "test failed")
+
+    """
+    This function tests the functionality of the convolutional decoder block by flipping 2 bits in random positions. 
+    """
+    def test_002_conv_dec(self):
+        # Generate random bits as message
+        message_bit_stream = open(r"message_bit_stream", "w+")
+
+        for i in range(2048):
+            bit = random.getrandbits(1)
+            message_bit_stream.writelines(str(bit) + "\n")
+
+        message_bit_stream.close()
+
+        # Encode the message
+        message_bit_stream = open(r"message_bit_stream", "r")
+        initial_message = tuple(map(int, message_bit_stream.readlines()))
+
+        message_bit_stream.close()
+
+        conv_enc = a3sat.conv_enc()
+        encoder = blocks.vector_source_b(initial_message, False, 1, [])
+        self.tb.connect(encoder, conv_enc)
+        encoder_output = blocks.vector_sink_b()
+        self.tb.connect(conv_enc, encoder_output)
+        self.tb.run()
+        encoded_message = encoder_output.data()
+
+        corrupted_message = self.flip_bits(encoded_message, 2)
+
+        # Decode the message
         conv_dec = a3sat.conv_dec(False)
         src = blocks.vector_source_b(corrupted_message, False, 1, [])
         self.tb.connect(src, conv_dec)
