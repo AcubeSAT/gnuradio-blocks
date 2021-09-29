1eb44ce6e5752a12e802ebaf4756a65e3fff2ad0
efthimia145 <efthimia145@gmail.com>
2021-09-29 17:28:13 +0300

    Delete extra files

--- gr-a3sat/python/CMakeFiles/pygen_python_e9d9e.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for pygen_python_e9d9e.
-# This may be replaced when dependencies are built.
--- gr-a3sat/python/qa_ldpc_enc_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export VOLK_GENERIC=1
-export GR_DONT_LOAD_PREFS=1
-export srcdir="/home/efthamar/CLionProjects/error-correction-codes/gr-a3sat/python"
-export GR_CONF_CONTROLPORT_ON=False
-export PATH="/home/efthamar/CLionProjects/error-correction-codes/gr-a3sat/python":$PATH
-export LD_LIBRARY_PATH="":$LD_LIBRARY_PATH
-export PYTHONPATH=/home/efthamar/CLionProjects/error-correction-codes/gr-a3sat/swig:$PYTHONPATH
-/usr/bin/python3 /home/efthamar/CLionProjects/error-correction-codes/gr-a3sat/python/qa_ldpc_enc.py 
