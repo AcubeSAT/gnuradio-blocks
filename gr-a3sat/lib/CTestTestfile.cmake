54375fb99c98a7015513f3834081ea8d14fab5c5
efthimia145 <efthimia145@gmail.com>
2021-09-29 18:01:12 +0300

    Delete more extra files

--- gr-a3sat/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/efthamar/CLionProjects/error-correction-codes/gr-a3sat/lib
-# Build directory: /home/efthamar/CLionProjects/error-correction-codes/gr-a3sat/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- gr-a3sat/swig/CMakeFiles/pygen_swig_c9e68.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for pygen_swig_c9e68.
-# This may be replaced when dependencies are built.
