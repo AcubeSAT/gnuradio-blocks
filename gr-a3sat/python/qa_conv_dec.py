9bc2adea893b5ca7ce7dcb1defaa3cc07194930a
efthimia145 <efthimia145@gmail.com>
2021-11-07 15:30:54 +0200

    Update test cases

--- gr-a3sat/python/qa_conv_dec.py
+++ gr-a3sat/python/qa_conv_dec.py
@@ -14,6 +14,7 @@ from gnuradio import blocks
 import a3sat_swig as a3sat
 import random
 
+
 class qa_conv_dec(gr_unittest.TestCase):
 
     def setUp(self):
@@ -26,10 +27,13 @@ class qa_conv_dec(gr_unittest.TestCase):
     This function tests the functionality of the convolutional decoder block.
     It initializes a stream of 2048 random generated bits.
     Encodes them using the convolutional encoder block. 
-    Then decodes the modified stream and compares it to the initial one. 
+    Each bit in the stream is then represented with a float value in the range of [0.0, 1.0].
+    O -> [0.0, 0.5] & 1 -> [0.5, 1.0] 
+    Extra noise is added in order to add some errors in the bit float representation. 
+    Then decodes the stream and compares it to the initial one.
     """
 
-    def test_soft_decoding(self):
+    def soft_decoding(self, num_of_flipped_bits):
         # Generate random bits as message
         encoder_bit_stream = []
         decoder_bit_stream = []
@@ -53,21 +57,56 @@ class qa_conv_dec(gr_unittest.TestCase):
 
         for i in range(codeword_length*2):
             if encoded_message[i] == 0:
-                decoder_bit_stream.append(round(random.uniform(0.0, 0.49), 4))
+                decoder_bit_stream.append(round(random.uniform(0.0, 0.5), 4))
             else:
                 decoder_bit_stream.append(round(random.uniform(0.5, 1.0), 4))
 
         decoder_message = tuple(decoder_bit_stream)
 
+        corrupted_message = self.add_noise(decoder_message, num_of_flipped_bits)
+
         # Decode the message
         conv_dec = a3sat.conv_dec(False)
-        src = blocks.vector_source_f(decoder_message, False, 1, [])
+        src = blocks.vector_source_f(corrupted_message, False, 1, [])
         self.tb.connect(src, conv_dec)
         dst = blocks.vector_sink_b()
         self.tb.connect(conv_dec, dst)
         self.tb.run()
         result_data = dst.data()
         self.assertTupleEqual(initial_message, result_data, "Test failed")
+
+    """
+    This function calls the add_noise function 3 times with different num_of_corrupted_bits param. 
+    """
+    def test_soft_decoding(self):
+        for i in range(3):
+            for num_of_errors in range(0, 4):
+                self.soft_decoding(num_of_errors)
+
+    """
+    A function that receives a message containing float representation of random bits and 
+    adds noise in a certain number of bits in random positions.
+    
+    Arguments
+    ---------
+    
+    message : tuple
+       the message to be modified
+    number_of_corrupted_bits: int
+       the number of bits to be corrupted with noise
+    """
+    def add_noise(self, message, number_of_corrupted_bits):
+        message = list(message)
+
+        for i in range(number_of_corrupted_bits):
+            position = random.randint(0, 2048)
+            noise = round(random.uniform(0.5, 1.0), 4)
+            if message[position] <= 0.5:
+                message[position] += noise
+            else:
+                message[position] -= noise
+
+        return tuple(message)
 
 
 if __name__ == '__main__':
